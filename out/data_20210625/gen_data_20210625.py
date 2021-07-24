d5fc2c652daef850e04dd31147cd06df909169b8
kunyang <kun.bj@foxmail.com>
2021-07-24 16:53:42 -0400

    version 0.0.3 1. update cnn_bert for refrigerator data (no GPU)    1) add refrigerator.py in datasets    2) modify two_stream_bert2_main.py

--- out/data_20210625/gen_data_20210625.py
+++
@@ -1,5 +0,0 @@
-import os
-
-cmd = 'python3.7 feature_extraction.py --video_list data/video.txt  --network vgg --framework tensorflow --output_path out/ --tf_model ./slim/vgg_16.ckpt'
-# eval(cmd)
-os.system(cmd)
