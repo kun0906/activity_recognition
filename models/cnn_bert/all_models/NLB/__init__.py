6da6c839a642cb9927007e76cc44cac2a8e0cccd
kunyang <kun.bj@foxmail.com>
2021-07-24 16:02:58 -0400

    version 0.0.2 1. update cnn_bert for refrigerator data (no GPU)    1) add refrigerator.py in datasets    2) modify two_stream_bert2_main.py

---
+++ models/cnn_bert/all_models/NLB/__init__.py
@@ -0,0 +1,1 @@
+from .NLBlockND import NLBlockND
---
+++ models/cnn_bert/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .ucf101 import ucf101
+from .hmdb51 import hmdb51
+from .refrigerator import refrigerator
+
+__all__ = ('ucf101', 'hmdb51', 'refrigerator')
